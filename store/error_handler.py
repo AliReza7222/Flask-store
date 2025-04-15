bfc8ca4b8a071193da344668c8118eee6430becf
AliReza7222 <ali.r.salehi99@gmail.com>
2025-04-15 18:21:20 +0430

    handeler error 404 created

--- store/error_handler.py
+++ store/error_handler.py
@@ -8,3 +8,7 @@ def store_error_handler(app):
     @app.errorhandler(ValidationError)
     def handle_validation_error(error):
         return jsonify({"errors": error.messages}), HTTPStatus.BAD_REQUEST
+
+    @app.errorhandler(404)
+    def handle_not_found_error(error):
+        return jsonify({"errors": error.description}), HTTPStatus.NOT_FOUND
