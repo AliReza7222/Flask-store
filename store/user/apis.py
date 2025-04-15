a010ee4b19f4efc8207e7770606da71978a4d28e
AliReza7222 <ali.r.salehi99@gmail.com>
2025-04-15 18:20:48 +0430

    Refactor: Separate business logic from API layer into service classes

--- store/user/apis.py
+++ store/user/apis.py
@@ -3,19 +3,15 @@ from http import HTTPStatus
 from flask import jsonify
 from flask.views import MethodView
 from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-    get_jwt_identity,
     jwt_required,
 )
 
-from store.extensions import db
 from store.routes import create_blueprint_api
-from store.user.models import User
 from store.user.schemas import LoginUserSchema, RegisterUserSchema
-from store.validators import exists_row
+from store.user.services import UserService
 
 blueprint = create_blueprint_api(name="users", url_prefix="users", version="v1")
+user_service = UserService()
 
 
 @blueprint.route("/")
@@ -23,47 +19,21 @@ class RegisterUserAPI(MethodView):
     @blueprint.arguments(RegisterUserSchema)
     @blueprint.response(HTTPStatus.CREATED, RegisterUserSchema)
     def post(self, data):
-        register_user_schema = RegisterUserSchema()
-        valid_data = register_user_schema.load(data)
-        user = register_user_schema.create_user(data=valid_data)
-
-        if exists_row(User, email=user.email):
-            return jsonify(
-                {"error": f"User with email {user.email} already exists."},
-            ), HTTPStatus.BAD_REQUEST
-
-        db.session.add(user)
-        db.session.commit()
-        return jsonify(register_user_schema.dump(user)), HTTPStatus.CREATED
+        return jsonify(user_service.register_user(data)), HTTPStatus.CREATED
 
 
 @blueprint.route("/login")
 class LoginUserAPI(MethodView):
     @blueprint.arguments(LoginUserSchema)
     def post(self, data):
-        login_user_schema = LoginUserSchema()
-        valid_data = login_user_schema.load(data)
-        user = User.query.filter_by(email=valid_data.get("email")).first()
-
-        if not user or not user.check_password(valid_data.get("password")):
-            return jsonify(
-                {"error": "email or password is invalid!"},
-            ), HTTPStatus.NOT_FOUND
-
-        response = {
-            "access_token": create_access_token(identity=user.email),
-            "refresh_token": create_refresh_token(identity=user.email),
-        }
-        return jsonify(response), HTTPStatus.OK
+        return jsonify(user_service.login_user(data)), HTTPStatus.OK
 
 
 @blueprint.route("/refresh")
 class RefreshTokenAPI(MethodView):
     @jwt_required(refresh=True)
     def post(self):
-        identity = get_jwt_identity()
-        access_token = create_access_token(identity=identity)
-        return jsonify(access_token=access_token)
+        return jsonify(access_token=user_service.refresh_token()), HTTPStatus.OK
 
 
 @blueprint.route("/me")
@@ -71,18 +41,4 @@ class UserDetailAPI(MethodView):
     @blueprint.response(HTTPStatus.OK)
     @jwt_required()
     def get(self):
-        user = User.query.filter_by(email=get_jwt_identity()).first()
-        if not user:
-            return jsonify(
-                {"error": "No exists this user!"},
-            ), HTTPStatus.NOT_FOUND
-        return jsonify(self.response(user)), HTTPStatus.OK
-
-    def response(self, user):
-        return {
-            "id": user.id,
-            "email": user.email,
-            "full_name": user.full_name,
-            "active": user.active,
-            "is_admin": user.is_admin,
-        }
+        return user_service.detail_user()
---
+++ store/user/services.py
@@ -0,0 +1,61 @@
+from http import HTTPStatus
+
+from flask import abort
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+)
+
+from store.extensions import db
+from store.user.models import User
+from store.user.schemas import LoginUserSchema, RegisterUserSchema
+from store.validators import exists_row
+
+
+class UserService:
+    def register_user(self, data):
+        register_user_schema = RegisterUserSchema()
+        valid_data = register_user_schema.load(data)
+        user = register_user_schema.create_user(data=valid_data)
+
+        if exists_row(User, email=user.email):
+            abort(
+                HTTPStatus.BAD_REQUEST,
+                description=f"User with email {user.email} already exists.",
+            )
+
+        db.session.add(user)
+        db.session.commit()
+        return register_user_schema.dump(user)
+
+    def login_user(self, data):
+        login_user_schema = LoginUserSchema()
+        valid_data = login_user_schema.load(data)
+        user = User.query.filter_by(email=valid_data.get("email")).first()
+
+        if not user or not user.check_password(valid_data.get("password")):
+            abort(
+                HTTPStatus.NOT_FOUND,
+                description="email or password is invalid!",
+            )
+        return {
+            "access_token": create_access_token(identity=user.email),
+            "refresh_token": create_refresh_token(identity=user.email),
+        }
+
+    def refresh_token(self):
+        identity = get_jwt_identity()
+        return create_access_token(identity=identity)
+
+    def detail_user(self):
+        user = User.query.filter_by(email=get_jwt_identity()).first()
+        if not user:
+            abort(HTTPStatus.NOT_FOUND, description="No exists this user!")
+        return {
+            "id": user.id,
+            "email": user.email,
+            "full_name": user.full_name,
+            "active": user.active,
+            "is_admin": user.is_admin,
+        }
