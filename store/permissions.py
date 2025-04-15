11e980c2c353d7d1cce2f8155fd876f5a44976b0
AliReza7222 <ali.r.salehi99@gmail.com>
2025-04-15 11:07:01 +0430

    fix permission admin_required for class base

--- store/permissions.py
+++ store/permissions.py
@@ -4,18 +4,19 @@ from http import HTTPStatus
 from flask import jsonify
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
-from store.user.models import User
-
 
 def admin_required():
     def decorator(func):
         @wraps(func)
         @jwt_required()
         def wrapper(*args, **kwargs):
+            from store.user.models import User
+
             user = User.query.filter_by(email=get_jwt_identity()).first()
             if not user or not user.is_admin:
                 return jsonify({"error": "Unauthorized"}), HTTPStatus.UNAUTHORIZED
-            return func(user, *args, **kwargs)
+            kwargs["user"] = user
+            return func(*args, **kwargs)
 
         return wrapper
 
